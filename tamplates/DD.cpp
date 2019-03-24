a00bd5bfe9bac918a877c62496cdc787eb760c0d
Oper_doc <s.lit@outlook.com>
2019-03-25 02:38:23 +0300

    last

--- tamplates/DD.cpp
+++ tamplates/DD.cpp
@@ -17,52 +17,40 @@ typedef int z;
 #define out cout 
 #define nl '\n'
 
-int mr(){return (rand() << 15) + rand();}
-struct nd{
-    int x, y, sz = 0, mmi;
-    nd *l, *r;
-    nd():l(nullptr),r(nullptr){}
-    nd(int x):x(x),y(mr()),l(nullptr),r(nullptr),sz(1){}
+struct ev{
+    z t, x, i;
+    ev(z t, z x, z i):t(t),x(x),i(i){} 
 };
-typedef nd* pn;
 
-void mer(pn& rt, pn l, pn r){
-    if(!l || !r) return void(rt = l ? l : r);
-    if(l->y < r->y) {mer(l->r, l->r, r); rt = l;}
-    else            {mer(r->l, l, r->l); rt = r;}
-}
-void spl(pn rt, pn& l, pn& r, int x){
-    if(!rt) return void(l = r = nullptr);
-    if(rt->x < x) {spl(rt->r, rt->r, r, x); l = rt;}
-    else          {spl(rt->l, l, rt->l, x); r = rt;}
-}
-void inter (pn& rt, pn l, pn r) {
-	if (!l || !r)  return void(rt = nullptr);
-	if (l->y < r->y)  swap (l, r);
-	pn Lt, Rt;
-	spl (r, Lt, Rt, l->x);
-    inter (l->l, l->l, Lt);
-	inter (l->r, l->r, Rt);
-    rt = l;
-}
-pn x1, x2 ,x3, rt1, rt2, rt3;
-void ins(pn& rt, int x){
-    spl(rt, x1, x2, x);
-    mer(rt, x1, new nd(x)); mer(rt, rt, x2);
-}
-
-void prt(pn rt){
-    if(!rt) return;
-    prt(rt->l);
-    out << rt->x << ' ';
-    prt(rt->r);
-}
 int main() {
-    z n, a;
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    z n, a, b;
     in >> n;
-    ve<z> ns1(n), ns2(n);
-    fo(i, n) {in >> a; ins(rt1, a);}
-    fo(i, n) {in >> a; ins(rt2, a);}
-    inter(rt3, rt1, rt2);
-    prt(rt3);
+    ve<ev> arr;
+    ve<pr<z, z>> ans(n, mp(-1, -1));
+    fo(i, n) {
+        in >> a >> b;
+        arr.pb(ev(b, a, i));
+    }
+    sort(al(arr), [&](ev &a, ev &b){return a.x < b.x;});
+    z curr = -1;
+    fo(i, n) {
+        if(curr == -1) {
+            curr = i;
+            ans[arr[curr].i].fs = arr[curr].x;
+            ans[arr[curr].i].sc = arr[curr].t;
+        } else {
+            if(arr[curr].t < arr[i].t) {
+                ans[arr[curr].i].sc = min(arr[i].x, ans[arr[curr].i].sc);
+                curr = i;
+                ans[arr[curr].i].fs = arr[curr].x;
+                ans[arr[curr].i].sc = arr[curr].t;
+            }
+        }
+    }
+    fo(i, n) {
+        out << ans[i].fs << ' ' << ans[i].sc << nl;
+    }
 }
